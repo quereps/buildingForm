97d9e98daadda11b29c21cc8b8fe088f3a12bc0b
quereps <quereps@gmail.com>
2020-03-19 10:53:04 +0000

    lkj

--- BuildingForm/search/index.js
+++ BuildingForm/search/index.js
@@ -1,69 +1,48 @@
 const searchModule = (function ($, ksAPI) {
     
     const getQIdentifierTextContain = function(a){
-	
         let result=[];
-        
-    
         for(var i in structure){
-    
             let string = JSON.stringify(structure[i].questionLabel);
-            
             if(string.indexOf(a)>0){
                 result.push(structure[i]);
             }
-            
-            
         }
         
         result.sort();
-        
         showResultModule.showInterface("searchResult",result);
-        
     }
 
     const getQIdentifierThatContain = function(a,b){
-	
-	
         var result=[];
         var listOfidentifiers = Object.keys(vpGetIdentifiersMap());
         
-        
-        
         for(var i in listOfidentifiers){
-    
             var string = JSON.stringify(listOfidentifiers[i]);
-            
+
             if(b){
-                
                 if(string.indexOf(a)>0 && string.indexOf(b)>0){
                     result.push(listOfidentifiers[i]);
                 }
-                
             }
-            
             else{
                 if(string.indexOf(a)>0){
                     result.push(listOfidentifiers[i]);
                 }
             }
-            
         }
-        
+
         result.sort();
         console.log(result);
         tableToConsole(result);
-        
     }
 
 	const init = function(){
 
-	}
-
-		
+    }
+    
 	return{
 		Start:function(){init()}
 	}
-	
 	
 })(jQuery, ksAPI);
