a4d88ad424df71a5acf5c1bef5494b15d7f7119d
quereps <quereps@gmail.com>
2020-03-24 11:23:20 +0000

    dsf

--- BuildingForm/summaryTable.js
+++ BuildingForm/summaryTable.js
@@ -2,8 +2,8 @@
 const summaryTableModule  = (function ($, ksAPI) {
 	
 	const init = function(){
-        buttonModule.NewType("sum","summaryTableModule.Launch()","sum");
-        buttonModule.New("sum","page","sum");
+        //buttonModule.NewType("sum","summaryTableModule.Launch()","sum");
+        //buttonModule.New("sum","page","sum");
 
         let sumButton = buttonModule.New("page","summaryTableModule.Launch()",["icon","sum"]);
 		sumButton.displayButton();
