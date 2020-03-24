d832b0e9444c806003cda0f53b7f329a936825fb
quereps <quereps@gmail.com>
2020-03-24 12:05:34 +0000

    fdsdsf

--- BuildingForm/interface/interface.js
+++ BuildingForm/interface/interface.js
@@ -40,7 +40,7 @@ const InterfaceModule  = (function ($, ksAPI) {
 	
 	const createInterface = function(){
 		console.log("createInterface");
-		jQuery("#main_frame").prepend("<div id='interface' style=\"display:none;\"><div class='menu'></div><div class='content'></div><div class='close' onclick='InterfaceModule.hide()'></div>");
+		jQuery("#main_frame").prepend("<div id='interface' class='hide'><div class='menu'></div><div class='content'></div><div class='close' onclick='InterfaceModule.hide()'></div>");
 	} 
 
 	const show = function(){
