e7c20aca237a350a076360c743f24e9e666e10d7
quereps <quereps@gmail.com>
2020-03-24 16:13:45 +0000

    dsfds

--- BuildingForm/search/search.js
+++ BuildingForm/search/search.js
@@ -5,7 +5,7 @@ const searchModule = (function ($, ksAPI) {
 
     const searchObj = function(){
         this.button = buttonModule.New("page","searchModule.Launch()",["icon","search"]);
-        this.form = "<label>Identifier</label><input type='text' onkeyup='searchModule:getQIdentifierTextContain(this)'></input>";
+        this.form = "<label>Identifier</label><input type='text' onkeyup='searchModule.getQIdentifierTextContain(this)'></input>";
 
         return this;
     }
